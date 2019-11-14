11cefa34b39a40c027d2b6d0df94d786a6960122
Michele Di Giovanni <michele.digiovanni@studenti.unicampania.it>
2019-11-14 12:07:43 +0100

    modifiche e correzioni Exy
    ho corretto l'head della funzione

--- script/Exy.m
+++ script/Exy.m
@@ -1,9 +1,9 @@
 %voglio il campo nei punti (x,y) dato dal filo nel punto P con densità di
 %carica lambda
 
-function res = Exy(P,x,y,lambda) 
-    xf = P[0]
-    yf = P[1]
-    eps0 = 8.85e-12
-    res = (lambda)./((2*pi*eps0)*sqrt((xf-xc)^2+(yf-yc)^2))
+function res = Exy(P,xc,yc,lambda) 
+    xf = P(1);
+    yf = P(2);
+    eps0 = 8.85e-12;
+    res = (lambda)./((2*pi*eps0)*sqrt((xf-xc)^2+(yf-yc)^2));
 end
