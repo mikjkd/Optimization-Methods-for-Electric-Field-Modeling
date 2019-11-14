8d89bd20c72b601b18e4aa65806c5b35e317a6d7
Michele Di Giovanni <michele.digiovanni@studenti.unicampania.it>
2019-11-14 12:03:44 +0100

    Update Exy.m

--- script/Exy.m
+++ script/Exy.m
@@ -1,6 +1,9 @@
+%voglio il campo nei punti (x,y) dato dal filo nel punto P con densità di
+%carica lambda
+
 function res = Exy(P,x,y,lambda) 
-    xf = P[0]
-    yf = P[1]
-    eps0 = 8.85e-12
-    res = (lambda)./((2*pi*eps0)*sqrt((xf-x)^2+(yf-y)^2))
+    xf = P(1);
+    yf = P(2);
+    eps0 = 8.85e-12;
+    res = (lambda)/((2*pi*eps0)*sqrt((xf-x)^2+(yf-y)^2));
 end
