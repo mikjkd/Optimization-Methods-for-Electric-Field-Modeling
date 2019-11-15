33456b9436315ce75dd97908359273f3c352d58a
Michele Di Giovanni <michele.digiovanni@studenti.unicampania.it>
2019-11-15 19:36:39 +0100

    fobj
    modifiche alla fobj e vari test

--- script/Exy.m
+++ script/Exy.m
@@ -5,5 +5,5 @@ function res = Exy(P,xc,yc,lambda)
     xf = P(1);
     yf = P(2);
     eps0 = 8.85e-12;
-    res = (lambda)./((2*pi*eps0)*sqrt((xf-xc)^2+(yf-yc)^2));
+    res = (lambda)./((2*pi*eps0)*sqrt((xf-xc).^2+(yf-yc).^2));
 end
