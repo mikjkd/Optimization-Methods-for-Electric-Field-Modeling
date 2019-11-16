2ee7cd50a918bf3f866cffd748ffaf73a326d39c
Michele Di Giovanni <micheledigio.mdg@gmail.com>
2019-11-16 13:52:05 +0100

    Edesiderata
    creazione del campo desiderato come Ededisedata

--- script/fobj.m
+++ script/fobj.m
@@ -13,10 +13,9 @@ P = [P1;P2;P3;P4];
 
 %funzione che fa la norma quadra del campo Exy dato da un solo filo
 %nell'intervallo [-0.05, 0.05]
-
+Edesiderata = Etot(P,xc,yc,[lambda(1),lambda(2),lambda(3),lambda(4)]);
 %scrittura funzione obj normalizzata
-fo =  @(lmbd1) (1/mean(Etot(P,xc,yc,[lambda(1),lambda(2),lambda(3),lambda(4)])))*...
-       sqrt((xb-xa)/length(xc))*norm( Etot(P,xc,yc,[lambda(1),lambda(2),lambda(3),lambda(4)]) - Etot(P,xc,yc,[lambda(1),lmbd1,lambda(3),lambda(4)]))
+fo =  @(lmbd1) (1/mean(Edesiderata))*sqrt((xb-xa)/length(xc))*norm(Edesiderata - Etot(P,xc,yc,[lambda(1),lmbd1,lambda(3),lambda(4)]))
 passo1 = 1e-08:0.0000000001:10e-08;
 foval(fo,passo1);
 foplot(fo,passo1);
