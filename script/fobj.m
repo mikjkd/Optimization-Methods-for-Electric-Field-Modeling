aca73cb50157f1661cb3943a9d4b3a269b6531d9
Michele Di Giovanni <micheledigio.mdg@gmail.com>
2019-11-17 11:34:11 +0100

    Edesiderata
    nuova Edesiderata, questa volta costante.
    subplot

--- script/fobj.m
+++ script/fobj.m
@@ -18,7 +18,8 @@ P = [P1;P2;P3;P4];
 
 %funzione che fa la norma quadra del campo Exy dato da un solo filo
 %nell'intervallo [-0.05, 0.05]
-Edesiderata = Etot(P,xc,yc,[lambda(1),lambda(2),lambda(3),lambda(4)]);
+%Edesiderata = Etot(P,xc,yc,[lambda(1),lambda(2),lambda(3),lambda(4)]);
+Edesiderata = 2.8563e+04;
 %scrittura funzione obj normalizzata
 %fo dipendende da una variabile lmbd1
 fo =  @(lmbd1) (1/mean(Edesiderata))*sqrt((xb-xa)/length(xc))*norm(Edesiderata - Etot(P,xc,yc,[lambda(1),lmbd1,lambda(3),lambda(4)]));
@@ -28,11 +29,16 @@ f1 = @(lmbd1,lmbd2) (1/mean(Edesiderata))*sqrt((xb-xa)/length(xc))*norm(Edesider
 %dove faccio variare lmbd1 per fare la ricerca del minimo in 1 variabile
 passo1 = 1e-08:0.000000001:10e-08;
 %%plot (X, Y)
-%plot(passo1,fo2Deval(fo,passo1));
+subplot(2,1,1)
+plot(passo1,fo2Deval(fo,passo1));
+grid
+title('Lambda 2');
 %mi creo i punti da graficare in 2 var
 vs = fo3Deval(f1,passo1,passo1);
-surf(passo1,passo1,vs);
-colorbar;
+subplot(2,1,2)
+surf(passo1,passo1,vs)
+title('Lambda 2 - Lambda 3');
+
 
 %plotto come faria fo al variare di lmbd1
 %foplot(fo,passo1);
