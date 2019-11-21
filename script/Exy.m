b0c982715d19f1ca3ffcad879d482830c1faa161
Michele Di Giovanni <michele.digiovanni@studenti.unicampania.it>
2019-11-21 12:23:15 +0100

    Eobj
    scrivo campo come differenza tra componenti

--- script/Exy.m
+++ script/Exy.m
@@ -1,9 +1,10 @@
 %voglio il campo nei punti (x,y) dato dal filo nel punto P con densità di
 %carica lambda
 
-function res = Exy(P,xc,yc,lambda) 
+function [Ex,Ey] = Exy(P,xc,yc,lambda) 
     xf = P(1);
     yf = P(2);
     eps0 = 8.85e-12;
-    res = (lambda)./((2*pi*eps0)*sqrt((xf-xc).^2+(yf-yc).^2));
+    Ex = (lambda.*(xf-xc))./((2*pi*eps0).*((xf-xc).^2+(yf-yc).^2));
+    Ey = (lambda.*(yf-yc))./((2*pi*eps0).*((xf-xc).^2+(yf-yc).^2));
 end
